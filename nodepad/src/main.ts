fc815ccfbd8c56d9ce647d0c3c25260b5c2bf9ef
hsyoon <hsyoon@wemb.co.kr>
2020-12-24 08:52:43 +0900

    불필요한 코드 제거

--- nodepad/src/main.ts
+++ nodepad/src/main.ts
@@ -2,10 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import vuetify from './plugins/vuetify'
-import vmodal from 'vue-js-modal'
 
 Vue.config.productionTip = false
-Vue.use(vmodal);
 
 new Vue({
   store,
